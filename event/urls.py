c17595e4271016a4eef4627106f179d677e58408
Vincent Ogbe <vincentogbe085@gmail.com>
2018-02-27 16:27:22 +0200

    Organiser profile and profiles

--- event/urls.py
+++ event/urls.py
@@ -22,6 +22,9 @@ urlpatterns = [
     url(r'^$', event.views.Home.as_view(), name='index'),
     url(r'^create/', event.views.CreateEvent.as_view(), name='create-event'),
     url(r'^manage/', event.views.ManageEvents.as_view(), name='manage-events'),
+    url(r'^organisers-profile/(?P<organiser_id>[0-9]+)$', event.views.OrganisersProfile.as_view(),
+        name='organisers-profile'),
     url(r'(?P<event_id>[0-9]+)$', event.views.ViewEvent.as_view(), name='view-event'),
-    url(r'^browse/',event.views.ListEvents.as_view(), name='browse-events')
+    url(r'^browse/',event.views.ListEvents.as_view(), name='browse-events'),
+    url(r'^organiser-profiles/',event.views.OrganiserProfiles.as_view(), name='organiser-profiles')
 ]
